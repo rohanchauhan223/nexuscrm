9e8b54df35a3b6aa7d9438ecaf0d6c6ad35e70e0
rohanchauhan223 <rohanchauhan670@gmail.com>
2026-04-07 22:39:47 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '2mb',
-    },
+  output: 'export',
+  images: {
+    unoptimized: true,
   },
 }
 
